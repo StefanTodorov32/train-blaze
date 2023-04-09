44a7fcc0fce670a69eeebb370f340a7eb229e169
StefanTodorov32 <bgtrak@gmail.com>
2023-04-09 09:46:02 +0300

    updated

--- src/components/Details/RatingBadge/RatingBadge.jsx
+++ src/components/Details/RatingBadge/RatingBadge.jsx
@@ -4,19 +4,14 @@ import { getAllRates } from "../../../services/rateService";
 
 const RatingBadge = ({ workoutId, setRatingArrays, ratingsArray, workout, renderBadge, setCanRate, userId }) => {
     const [avgRating, setAvgRating] = useState(0)
+    let ratingCount = 0
     useEffect(() => {
         getAllRates(workoutId)
             .then(r => {
-                console.log("🚀 ~ file: RatingBadge.jsx:27 ~ useEffect ~ workoutId:", workoutId)
-                console.log("🚀 ~ file: RatingBadge.jsx:15 ~ a ~ ratingsArray:", ratingsArray)
-                console.log("🚀 ~ file: RatingBadge.jsx:13 ~ useEffect ~ a:", a)
-                let ratingCount = 0
                 ratingsArray.map(r => {
                     ratingCount += r.rate
                 })
                 setAvgRating(ratingCount / ratingsArray.length)
-                console.log("🚀 ~ file: RatingBadge.jsx:26 ~ useEffect ~ ratingsArray.length:", ratingsArray.length)
-                console.log("🚀 ~ file: RatingBadge.jsx:27 ~ useEffect ~ ratingCount:", ratingCount)
             })
     }, [workout, renderBadge, workoutId, userId])
 
